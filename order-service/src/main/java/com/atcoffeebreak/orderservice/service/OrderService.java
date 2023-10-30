e3f8cfbea709da46080e25a5f533ea08b17b13c2
Rodrigo Soares Fragozo <rsfragozo@gmail.com>
2023-10-30 14:08:37 +0100

    Adding discovery server integration

--- order-service/src/main/java/com/atcoffeebreak/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/atcoffeebreak/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = OrderMapper.mapToOrder(orderRequest);
@@ -31,8 +31,8 @@ public class OrderService {
                 .map(OrderLineItem::getSkuCode)
                 .toList();
 
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
